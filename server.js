c5f8bfeee62bb16c2af193d376f08b605c9004f1
ashway <kostya.kzo@gmail.com>
2019-07-12 13:36:17 +0500

    api/uploadImages

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const fs = require('fs');
 const fastify = require('fastify')({ logger: { level: 'error' } });
 const moment = require('moment');
 const Next = require('next');
-const TelegramBot = require('node-telegram-bot-api');
+const Telegraf = require('telegraf')
 const _ = require('lodash');
-const concat = require('concat-stream');
 const pump = require('pump');
 const uuid = require('uuid/v1');
+
+const SocksAgent = require('socks5-https-client/lib/Agent');
+const socksAgent = new SocksAgent({
+    socksHost: '51.79.54.137',
+    socksPort: '6900',
+    socksUsername: 'proxyuser',
+    socksPassword: 'proxy!pass'
+});
 
 fastify.register(require('fastify-multipart'));
 
@@ -22,6 +29,8 @@ fastify.register(require('fastify-cors'), {
 const botTokenDev = '659727031:AAFZ_8AXLTu2bBSej7_2UN5ujjlVQPsLggk';
 const botToken = '556896286:AAH791dcePJHlImFEs3HYryHa8HDRljMyW4';
 
+let bot = new Telegraf(botTokenDev, { telegram: { agent: socksAgent } });
+
 fastify.register((fastify, opts, next) => {
   const app = Next({ dev });
   app
@@ -35,14 +44,14 @@ fastify.register((fastify, opts, next) => {
         })
       }
 
-        fastify.post('/uploadUserImage', async(req, reply) => {
+        fastify.post('/api/uploadUserImage', async(req, reply) => {
             let fields = {files: []};
 
             let tempDir = uuid();
-            fs.mkdirSync(`./uploadedCars/${tempDir}`);
+            fs.mkdirSync(`./static/uploadImg/${tempDir}`);
             const mp = req.multipart((field, file, filename) => {
                 fields.files.push(filename);
-                pump(file, fs.createWriteStream(`./uploadedCars/${tempDir}/${filename}`));
+                pump(file, fs.createWriteStream(`./static/uploadImg/${tempDir}/${filename}`));
             }, async function (err) {
                 if (err) {
                     reply.send(err);
@@ -50,19 +59,21 @@ fastify.register((fastify, opts, next) => {
                 }
 
                 fields.phone = fields.phone.replace(/\s+|\+|\(|\)/g, '');
-                let bot = new TelegramBot(botTokenDev, {
-                    polling: true
+                //try {
+                bot.telegram.sendMessage(-1001204370141, `Зарегистрировался новый водитель!\n${fields.name}\n${fields.phone}\nОжидаемый гонорар: ${fields.price} руб.`, { parse_mode: 'HTML' });
+                let mediaGroup = [];
+                _.forEach(fields.files, file=>{
+                    mediaGroup.push({
+                        'media': `https://dev.lux-motor.ru/static/uploadImg/${tempDir}/${file}`,
+                        'type': 'photo'
+                    });
                 });
-
-                try {
-                    let mediaGroup = [];
-                    _.forEach(fields.files, file=>mediaGroup.push({ type: 'photo', media: `attach://./uploadedCars/${tempDir}/${file}`}));
-                    await bot.sendMediaGroup(-1001204370141, mediaGroup);
-                } catch(err) {
-                    console.log(err.code);
-                }
-
+                bot.telegram.sendMediaGroup(-1001204370141, mediaGroup);
+                bot.launch();
                 //-- Закончили получать форму
+                /*} catch(err) {
+                    console.log(err);
+                }*/
                 reply.code(200).send();
             });
 
@@ -72,15 +83,13 @@ fastify.register((fastify, opts, next) => {
         });
 
       fastify.post('/api/sendRequest',  async(req, reply) => {
-        let bot = new TelegramBot(botToken, {
-          polling: true
-        });
         let data = req.body;
         try {
           let dt = moment().utc().utcOffset(5).format('DD.MM.YYYY HH:mm');
-          await bot.sendMessage(-1001397757254, `${dt}\n${data.fio}\n<a href="tel:${data.phone}">${data.phone}</a>`, { parse_mode: 'HTML' });
+          bot.telegram.sendMessage(-1001397757254, `${dt}\n${data.fio}\n<a href="tel:${data.phone}">${data.phone}</a>`, { parse_mode: 'HTML' });
+          bot.launch();
         } catch(err) {
-          //console.log(err.code);
+          console.log(err.code);
         }
 
         reply.header('Content-Type', 'application/json').code(200);
