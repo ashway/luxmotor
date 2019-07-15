e720f651eb6def356c9db90318952f8dce412b7e
ashway <kostya.kzo@gmail.com>
2019-07-15 13:41:24 +0500

    Подготовка номера телефона в телеграм боте

--- server.js
+++ server.js
@@ -57,11 +57,10 @@ fastify.register((fastify, opts, next) => {
                     reply.send(err);
                     return
                 }
-
+                let dt = moment().utc().utcOffset(5).format('DD/MM/YYYY HH:mm');
                 fields.phoneRaw = fields.phone.replace(/\s+|\+|\(|\)/g, '');
-                fields.phone = fields.phone.replace(/\s+/g, '-');
                 //try {
-                bot.telegram.sendMessage(-1001204370141, `Зарегистрировался новый водитель!\n${fields.name}\n<a href="tel:${fields.phoneRaw}">${fields.phone}\nОжидаемый гонорар: ${fields.price} руб.`, { parse_mode: 'HTML' });
+                bot.telegram.sendMessage(-1001204370141, `${dt}\nЗарегистрировался новый водитель!\n${fields.name}\n<a href="tel:${fields.phoneRaw}">${fields.phone}\nОжидаемый гонорар: ${fields.price} руб.`, { parse_mode: 'HTML' });
                 let mediaGroup = [];
                 _.forEach(fields.files, file=>{
                     mediaGroup.push({
@@ -86,9 +85,8 @@ fastify.register((fastify, opts, next) => {
       fastify.post('/api/sendRequest',  async(req, reply) => {
         let data = req.body;
         try {
-          let dt = moment().utc().utcOffset(5).format('DD.MM.YYYY HH:mm');
+          let dt = moment().utc().utcOffset(5).format('DD/MM/YYYY HH:mm');
           data.phoneRaw = fields.phone.replace(/\s+|\+|\(|\)/g, '');
-          data.phone = fields.phone.replace(/\s+/g, '-');
           bot.telegram.sendMessage(-1001397757254, `${dt}\n${data.fio}\n<a href="tel:${data.phoneRaw}">${data.phone}</a>`, { parse_mode: 'HTML' });
           bot.launch();
         } catch(err) {
